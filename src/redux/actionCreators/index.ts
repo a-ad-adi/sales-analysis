21ee9acb5a427695e01446a7d9991ce4e0ef883b
Aditya TIlyalkar <adityat16@Adityas-MacBook-Pro-2.local>
2022-11-07 08:14:48 -0800

    minor fix

--- src/redux/actionCreators/index.ts
+++ src/redux/actionCreators/index.ts
@@ -1,8 +1,8 @@
 import { Action, ActionType } from "redux/actionTypes";
 import { Dispatch } from "redux";
-import { fetchDashboard } from "stubs/fetchDashboard";
+import { fetchInventoryData } from "stubs/fetchInventoryData";
 
 export const fetchSalesData = () => async (dispatch: Dispatch<Action>) => {
-  const data = await fetchDashboard();
+  const data = await fetchInventoryData();
   dispatch({ type: ActionType.FETCH_INVENTORY_DATA, payload: data });
 };
